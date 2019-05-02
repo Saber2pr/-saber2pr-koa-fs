712e2685046d81cbec9bd224789f828ac2d375e8
saber2pr <saber2pr@gmail.com>
2019-05-02 19:59:23 +0000

    update

--- src/core/saber-koa-fs.ts
+++ src/core/saber-koa-fs.ts
@@ -2,24 +2,34 @@
  * @Author: saber2pr
  * @Date: 2019-04-27 19:41:46
  * @Last Modified by: saber2pr
- * @Last Modified time: 2019-04-27 20:20:08
+ * @Last Modified time: 2019-04-30 22:41:07
  */
-import { FS } from '@saber2pr/node'
+import { FS, Http } from '@saber2pr/node'
 import { Job, compose } from '@saber2pr/koa'
 
 export const IndexJob: Job = async (ctx, next) => {
   if (ctx.request.url === '/') {
-    ctx.response.end(await FS.readFile(process.cwd() + '/index.html'))
+    ctx.response.setHeader('Location', '/index.html')
+    ctx.response.statusCode = 302
+    ctx.response.end()
   } else {
     await next()
   }
 }
 
 export const FsJob: Job = async (ctx, next) => {
-  const files = await FS.search()
-  const target = process.cwd() + decodeURIComponent(ctx.request.url)
-  if (files.includes(target)) {
-    ctx.response.end(await FS.readFile(target))
+  if (ctx.request.url !== '/') {
+    try {
+      try {
+        const target = process.cwd() + decodeURIComponent(ctx.request.url)
+        const res = await FS.readFile(target)
+        ctx.response.end(res)
+      } catch (error) {
+        throw new Http.Exception(encodeURIComponent(error.message), 404)
+      }
+    } catch (error) {
+      Http.Exception.resolve(error, ctx.response)
+    }
   } else {
     await next()
   }
